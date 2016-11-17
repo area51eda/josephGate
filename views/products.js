2acd3bb20cc721154b70e8194481ea50574217d4
zimsteve <zimsteve@live.com>
2016-11-18 12:07:53 +1300

    fix master

--- views/products.js
+++ views/products.js
@@ -1,7 +1,11 @@
 var h = require('hyperscript')
 
 module.exports = ({products}, dispatch) => {
-  return h('div#products', {})   
+  return h('div#products', {}, products.map((product, index) => {
+    return [
+      h('p', {className: 'name'}, product.name),
+      h('p', "Price: ", product.price)
+    ]
+
+  }))
 }
-
-
