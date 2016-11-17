d333453ba5e479566fc3b1d90f3ac95b39918078
michael-lowe-nz <mikkeyfluttie@gmail.com>
2016-11-18 12:16:36 +1300

    increment and decrement tests written

--- test/index.js
+++ test/index.js
@@ -3,3 +3,57 @@ var freeze = require('deep-freeze')
 
 var reducer = require('../reducer')
 
+test("base test", function(t){
+  t.ok(true)
+  t.end()
+})
+
+test("Test that reducer can increment votes", function(t){
+  // arrange
+  const state = {
+    conspiracies: [
+      {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
+      {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'mick'},
+      {id: 3, description: 'joseph can play the flute', votes: 0, author: 'mick'}
+    ]
+  }
+
+  const expectedState = {
+    conspiracies: [
+      {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
+      {id: 2, description: 'joseph has 11 toes', votes: 1, author: 'mick'},
+      {id: 3, description: 'joseph can play the flute', votes: 0, author: 'mick'}
+    ]
+  }
+  freeze(state)
+  // act
+  const actual = reducer(state, {type: 'increment', payload: 1})
+  // assert
+  t.deepEquals(actual, expectedState, 'the reducer increments correctly')
+  t.end()
+})
+
+test("Test that reducer can decrement votes", function(t){
+  // arrange
+  const state = {
+    conspiracies: [
+      {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
+      {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'mick'},
+      {id: 3, description: 'joseph can play the flute', votes: 0, author: 'mick'}
+    ]
+  }
+
+  const expectedState = {
+    conspiracies: [
+      {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
+      {id: 2, description: 'joseph has 11 toes', votes: -1, author: 'mick'},
+      {id: 3, description: 'joseph can play the flute', votes: 0, author: 'mick'}
+    ]
+  }
+  freeze(state)
+  // act
+  const actual = reducer(state, {type: 'decrement', payload: 1})
+  // assert
+  t.deepEquals(actual, expectedState, 'the reducer decrements correctly')
+  t.end()
+})
