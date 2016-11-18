c251905f1937a89845af4b677736cd8e02840401
Steve Sharp <zimsteve@live.com>
2016-11-18 15:30:15 +1300

    Updates

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ main.appendChild(app)
 
 const initialState = {
   conspiracies: [
-    {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
-    {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'jim'},
-    {id: 3, description: 'joseph can play the flute', votes: 0, author: 'jack'}
+    {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick66'},
+    {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'jim_likes69'},
+    {id: 3, description: 'joseph can play the flute', votes: 0, author: 'jacko_wacko'},
+    {id: 4, description: 'joseph was born on the moon', votes: 0, author: 'sarah_bukka'},
+    {id: 5, description: 'joseph once killed a man in Reno', votes: 0, author: 'jim_like70'},
+    {id: 6, description: 'joseph was conceived by 2 trees, and somehow came out of a fish', votes: 0, author: 'andy_handy_dandy'}
   ]
 }
 
