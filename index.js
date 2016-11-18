c30d0ee0f2a2fd376835ba3efb79723e6e3b0cd2
Steve Sharp <zimsteve@live.com>
2016-11-18 15:19:02 +1300

    Updates

---
+++ index.js
@@ -0,0 +1,34 @@
+const redux = require('redux')
+const h = require('hyperscript')
+const morphdom = require('morphdom')
+
+const reducer = require('./reducer')
+const Theories = require('./views/Theories')
+
+const main = document.querySelector('main')
+const app = document.createElement('div')
+main.appendChild(app)
+
+const initialState = {
+  conspiracies: [
+    {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick'},
+    {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'jim'},
+    {id: 3, description: 'joseph can play the flute', votes: 0, author: 'jack'}
+  ]
+}
+
+let store = redux.createStore(reducer, initialState)
+
+store.subscribe(render)
+
+const dispatch = store.dispatch
+
+function render () {
+  const state = store.getState()
+  const newView = h('div#app', {}, [
+    Theories(state.conspiracies, dispatch)
+  ])
+  morphdom(app, newView)
+}
+
+store.dispatch({type: 'INIT'})
---
+++ reducer.js
@@ -0,0 +1,38 @@
+var clone = require('clone')
+
+module.exports = function reducer (state, action) {
+  var newState = clone(state)
+  var {conspiracies} = newState
+  var {type, payload} = action
+  switch (type) {
+    case 'INIT':
+      return newState
+    case 'INCREMENT_VOTES':
+      conspiracies.forEach((conspiracy, index) => {
+        if (conspiracy.id === payload) conspiracies[index].votes++
+      })
+      return newState
+    case 'DECREMENT_VOTES':
+      conspiracies.forEach((conspiracy, index) => {
+        if (conspiracy.id === payload) conspiracies[index].votes--
+      })
+      return newState
+    case 'ADD_CONSPIRACY':
+      if (payload) {
+        var ID = conspiracies[conspiracies.length - 1].id + 1
+        conspiracies.push({id: ID, description: payload.description, votes: 0, author: payload.author})
+      }
+      return newState
+    case 'DELETE_CONSPIRACY':
+      newState.conspiracies = conspiracies.filter((conspiracy) => {
+        return conspiracy.id !== payload
+      })
+      return newState
+    case 'EDIT_CONSPIRACY':
+      conspiracies.forEach((conspiracy) => {
+        if (conspiracy.id === payload.id) conspiracy.description = payload.description
+      })
+      return newState
+  }
+  return newState
+}
