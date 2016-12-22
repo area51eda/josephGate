0cc08658ef955a6a5fce9beb73837736877d2886
JasmineGWT <jazzgwwt@gmail.com>
2016-12-22 17:20:30 +1300

    Added voting logic so user can only vote once

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const Theories = require('./views/Theories')
 
 const initialState = {
   conspiracies: [
-    {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick66'},
-    {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'jim_likes69'},
-    {id: 3, description: 'joseph can play the flute', votes: 0, author: 'jacko_wacko'},
-    {id: 4, description: 'joseph was born on the moon', votes: 0, author: 'sarah_bukka'},
-    {id: 5, description: 'joseph once killed a man in Reno', votes: 0, author: 'jim_like70'},
-    {id: 6, description: 'joseph was conceived by 2 trees, and somehow came out of a fish', votes: 0, author: 'andy_handy_dandy'}
+    {id: 1, description: 'joseph is a robot', votes: 0, author: 'mick66', upVoted: false, downVoted: false},
+    {id: 2, description: 'joseph has 11 toes', votes: 0, author: 'jim_likes69', upVoted: false, downVoted: false},
+    {id: 3, description: 'joseph can play the flute', votes: 0, author: 'jacko_wacko', upVoted: false, downVoted: false},
+    {id: 4, description: 'joseph was born on the moon', votes: 0, author: 'sarah_bukka', upVoted: false, downVoted: false},
+    {id: 5, description: 'joseph once killed a man in Reno', votes: 0, author: 'jim_like70', upVoted: false, downVoted: false},
+    {id: 6, description: 'joseph was conceived by 2 trees, and somehow came out of a fish', votes: 0, author: 'andy_handy_dandy', upVoted: false, downVoted: false}
   ]
 }
 
